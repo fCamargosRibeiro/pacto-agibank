7f79c58e24e5ecf59fc105750cc254d60a47b2e3
Leandro Dutra <leandrosdutra@hotmail.com>
2019-12-15 08:44:27 -0300

    Refatoracao das entidades

--- src/main/java/com/pacto/alegre/empreend/controller/BoletimController.java
+++ src/main/java/com/pacto/alegre/empreend/controller/BoletimController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/v1")
+@CrossOrigin
 public class BoletimController {
 
     private final BoletimService boletimService;
